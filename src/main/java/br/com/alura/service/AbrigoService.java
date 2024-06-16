ac49defac453d1b969a7e4d9b53c215448584d67
Vinicius Serra <vinicius.serra14@gmail.com>
2024-06-16 15:57:43 -0300

    feat:finalizando principios SOLID na refatoração

--- src/main/java/br/com/alura/service/AbrigoService.java
+++ src/main/java/br/com/alura/service/AbrigoService.java
@@ -1,23 +1,32 @@
 package br.com.alura.service;
 
+import br.com.alura.client.ClientHttpConfiguration;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class AbrigoService {
 
+    private final ClientHttpConfiguration client;
+
+    public AbrigoService(ClientHttpConfiguration client){
+        this.client =  client;
+    }
+
+
+    ClientHttpConfiguration clientHttpConfiguration = new ClientHttpConfiguration();
+
+
     public  void cadastrarAbrigo() throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
         String uri = "http://localhost:8080/abrigos";
-        HttpResponse<String> response =  requestGet(uri, client);
+
+        HttpResponse<String> response = client.requestGet(uri);
+
         String responseBody = response.body();
         JsonArray jsonArray = JsonParser.parseString(responseBody).getAsJsonArray();
         System.out.println("Abrigos cadastrados:");
@@ -43,15 +52,10 @@ public class AbrigoService {
         json.addProperty("telefone", telefone);
         json.addProperty("email", email);
 
-        HttpClient client = HttpClient.newHttpClient();
         String uri = "http://localhost:8080/abrigos";
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(uri))
-                .header("Content-Type", "application/json")
-                .method("POST", HttpRequest.BodyPublishers.ofString(json.toString()))
-                .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response =  client.requestPost(uri, json);
+
         int statusCode = response.statusCode();
         String responseBody = response.body();
         if (statusCode == 200) {
@@ -63,23 +67,4 @@ public class AbrigoService {
         }
     }
 
-
-
-    private HttpResponse<String> requestGet(String uri,HttpClient client) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(uri))
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
-    }
-
-    private HttpResponse<String> requestPost(HttpClient client,String uri,JsonObject json) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(uri))
-                .header("Content-Type", "application/json")
-                .method("POST", HttpRequest.BodyPublishers.ofString(json.toString()))
-                .build();
-
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
-    }
 }
